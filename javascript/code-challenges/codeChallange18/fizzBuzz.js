a8cc80e251c2a2972cf783a6728f893335d9c52a
Laith-Saleem <67604121+Laith-Vlad@users.noreply.github.com>
2023-07-26 17:18:36 +0300

    Update fizzBuzz.js

--- javascript/code-challenges/codeChallange18/fizzBuzz.js
+++ javascript/code-challenges/codeChallange18/fizzBuzz.js
@@ -1,66 +1,68 @@
+
+'use strict';
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.children = [];
-        this.results = [];
-    }
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+    this.results = [];
+  }
 }
 
 function fizzBuzz(number) {
-    let output = '';
-    if (number % 3 === 0) {
-        output += 'fizz';
-    } 
-    if (number % 5 === 0) {
-        output += 'buzz';
-    } 
+  let output = '';
+  if (number % 3 === 0) {
+    output += 'fizz';
+  }
+  if (number % 5 === 0) {
+    output += 'buzz';
+  }
 
-    if (output === '') {
-        output = number.toString();
-    }
+  if (output === '') {
+    output = number.toString();
+  }
 
-    return output;
+  return output;
 }
 
 class fizzBuzzTree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value, parentValue) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      const parentNode = this.findNode(parentValue, this.root);
+      if (parentNode) {
+        parentNode.children.push(newNode);
+      } else {
+        throw new Error("Parent node not found.");
+      }
     }
+    newNode.results.push(fizzBuzz(value));
+  }
 
-    insert(value, parentValue) {
-        const newNode = new Node(value);
+  findNode(value, node) {
+    if (!node) return null;
+    if (node.value === value) return node;
 
-        if (!this.root) {
-            this.root = newNode;
-        } else {
-            const parentNode = this.findNode(parentValue, this.root);
-            if (parentNode) {
-                parentNode.children.push(newNode);
-            } else {
-                throw new Error("Parent node not found.");
-            }
-        }
-        newNode.results.push(fizzBuzz(value));
+    let result = null;
+    for (const child of node.children) {
+      result = this.findNode(value, child);
+      if (result) break;
     }
+    return result;
+  }
 
-    findNode(value, node) {
-        if (!node) return null;
-        if (node.value === value) return node;
-
-        let result = null;
-        for (const child of node.children) {
-            result = this.findNode(value, child);
-            if (result) break;
-        }
-        return result;
+  traverseDFS(node = this.root) {
+    if (!node) return;
+    console.log(`Node: ${node.value}, FizzBuzz: ${node.results.join(', ')}`);
+    for (const child of node.children) {
+      this.traverseDFS(child);
     }
-
-    traverseDFS(node = this.root) {
-        if (!node) return;
-        console.log(`Node: ${node.value}, FizzBuzz: ${node.results.join(', ')}`);
-        for (const child of node.children) {
-            this.traverseDFS(child);
-        }
-    }
+  }
 }
 module.exports = fizzBuzzTree;
