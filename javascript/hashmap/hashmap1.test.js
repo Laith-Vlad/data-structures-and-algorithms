fcc07632717ba139dcb3d2d3571ccabae1996080
Laith-Vlad <laithxfg13@gmail.com>
2023-09-13 17:27:28 +0000

    leftJoin

--- javascript/hashmap/hashmap1.test.js
+++ javascript/hashmap/hashmap1.test.js
@@ -5,27 +5,31 @@ const Hashmab = require("../code-challenges/challange30/Hashmab");
 const hashmapLeftJoin = require("./hashmapjoing");
 
 let hashMapA = new Hashmab(1000);
-hashMapA.set("fond", "enamored");
-hashMapA.set("wrath", "anger");
-hashMapA.set("diligent", "employed");
-hashMapA.set("outfil", "garb");
-hashMapA.set("guide", "usher");
+
+hashMapA.set("apple", "red");
+hashMapA.set("banana", "yellow");
+hashMapA.set("cherry", "red");
+hashMapA.set("date", "brown");
+hashMapA.set("elderberry", "purple");
 
 let hashMapB = new Hashmab(1000);
-hashMapB.set("fond", "averse");
-hashMapB.set("wrath", "delight");
-hashMapB.set("diligent", "idle");
-hashMapB.set("guide", "follow");
-hashMapB.set("flow", "jam");
 
-describe("hashmap-left-join", () => {
-  it("all the values in the first hashmap are returned, and if values exist in the right hashmap ", () => {
-    expect(hashmapLeftJoin(hashMapA, hashMapB)).toStrictEqual([
-      ["guide", "usher", "follow"],
-      ["fond", "enamored", "averse"],
-      ["wrath", "anger", "delight"],
-      ["outfil", "garb", null],
-      ["diligent", "employed", "idle"],
-    ]);
+hashMapB.set("apple", "fruit");
+hashMapB.set("banana", "fruit");
+hashMapB.set("cherry", "fruit");
+hashMapB.set("date", "fruit");
+hashMapB.set("fig", "fruit");
+
+describe("Testing the hashmapLeftJoin function with different data", () => {
+  it("should return an array of arrays with values from the first hashmap and matching values from the second hashmap", () => {
+    expect(hashmapLeftJoin(hashMapA, hashMapB)).toEqual(
+      expect.arrayContaining([
+        ["apple", "red", "fruit"],
+        ["banana", "yellow", "fruit"],
+        ["cherry", "red", "fruit"],
+        ["date", "brown", "fruit"],
+        ["elderberry", "purple", null],
+      ])
+    );
   });
 });
--- javascript/hashmap/readme.md
+++ javascript/hashmap/readme.md
@@ -1,1 +1,8 @@
-asdadasdadas
+# Hashmap join 
+
+## Whiteboard Process
+
+
+![whitebord](./Untitled(4).jpg)
+
+npm test hashmap1.test.js
